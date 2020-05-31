82d0798af0cfed95936739276b6406899e215bc7
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2020-05-31 12:23:11 -0300

    chore(react-feather-icons): create-react-app cleanup

---
+++ svg-icons/src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <React.StrictMode>
+      <div className="App">
+        <h1>SVG Icons</h1>
+      </div>
+    </React.StrictMode>
+  );
+}
+
+export default App;
